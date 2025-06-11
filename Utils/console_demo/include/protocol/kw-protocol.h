e7c1f9f0be1a8d98c3807a6b433aa6257559bf6e
Myzhencai <Myzhencai@hotmail.com>
2025-06-11 18:43:01 +0800

    添加console_demo

---
+++ Utils/console_demo/include/protocol/kw-protocol.h
@@ -0,0 +1,39 @@
+﻿#ifndef kw_protocol_h
+#define kw_protocol_h
+
+#include "../kw-common.h"
+
+NS_KW_BEGIN
+
+enum class SensorOrd
+{
+	NONE_ORD = 0,
+	START_CAPTURE_ONCE = 1,
+	START_CAPTURE_MUTI = 2,
+	STOP_CAPTURE = 3
+};
+
+
+
+class Protocol
+{
+public:
+    virtual int encode(SensorOrd cmd, uint8_t *outputCmd, size_t* outputLen) = 0;
+    virtual int decode(const uint8_t *inputData, size_t inputLen, float *outputData) = 0;
+};
+
+class ProtocolCreator
+{
+public:
+	virtual std::shared_ptr<Protocol> createProtocol() const = 0;
+};
+
+class  HeadTailProtocolCreator final : public ProtocolCreator
+{
+public:
+	virtual std::shared_ptr<Protocol> createProtocol() const;
+};
+
+NS_KW_END
+
+#endif
