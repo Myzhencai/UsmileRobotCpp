fa712e97c91f7bc815fd88189f53837bbac2d98d
Myzhencai <Myzhencai@hotmail.com>
2025-06-11 18:49:55 +0800

    修改ForceMonitor

--- ForceMonitor/console_demo/include/kw-lib-all.h
+++
@@ -1,8 +0,0 @@
-﻿#ifndef kw_lib_all_h
-#define kw_lib_all_h
-
-#include "io/kw-io.h"
-#include "protocol/kw-protocol.h"
-#include "sensor/kw-sensor.h"
-
-#endif
--- ForceMonitor/console_demo/lib/linux_amd64/serialRun.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-
-serial_port_name="/dev/ttyUSB0"
-
-sudo chmod 666 ${serial_port_name}
-
-./KWTestApp
